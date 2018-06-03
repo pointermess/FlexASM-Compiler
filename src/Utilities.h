defeccc2435b1df1a69536a464392bc83283b1b2
pointermess <deskbuddybeta@gmail.com>
2018-06-03 18:36:31 +0200

    added in_set

--- src/Utilities.h
+++ src/Utilities.h
@@ -1,9 +1,20 @@
 #pragma once
 
 #include <string>
+#include <set>
+#include <vector>
 
 bool is_number(const std::string& s);
 
 void str_to_lower(std::string & s);
 
 std::string read_file(std::string fileName);
+
+template <typename T>
+bool in_set(std::set<T> & set, T value);
+
+template<typename T>
+inline bool in_set(std::set<T>& set, T value)
+{
+    return set.find(value) != set.end();
+}
