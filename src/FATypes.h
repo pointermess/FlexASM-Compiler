1cd848541929d08ade66948550421c76fedb620f
pointermess <deskbuddybeta@gmail.com>
2018-10-29 00:50:23 +0100

    added hack for visual studio formatting error and pseudo instruction stuff

--- src/FATypes.h
+++ src/FATypes.h
@@ -3,16 +3,11 @@
 #include <string>
 #include <vector>
 
-namespace FlexASM
-{
+namespace FlexASM {
 
-    enum MemorySize {
-        msUndefined = 0,
-        msByte = 1,
-        msWord = 2,
-        msDWord = 4
-    };
+    enum MemorySize { msUndefined = 0, msByte = 1, msWord = 2, msDWord = 4 };
 
+    // comma on new line is to fix buggy visual studio formatting......
     enum Register {
         farUnknown = -1,
         // General purpose registers
@@ -22,44 +17,45 @@ namespace FlexASM
         farAL = 0, // mod 4 = 0
         farBL = 1, // mod 4 = 1
         farCL = 2, // mod 4 = 2
-        farDL = 3, // mod 4 = 3
-
-                   // Significant byte of lower byte-pair 0000[00]00
-                   // floor(n / 4) = 1
-                   farAH = 4, // mod 4 = 0
-                   farBH = 5, // mod 4 = 1
-                   farCH = 6, // mod 4 = 2
-                   farDH = 7, // mod 4 = 3
-
-                              // Two most significant bytes [0000]0000
-                              // floor(n / 4) = 2
-                              farAX = 8,  // mod 4 = 0
-                              farBX = 9,  // mod 4 = 1
-                              farCX = 10, // mod 4 = 2
-                              farDX = 11, // mod 4 = 3
-
-                                          // All four bytes [00000000]
-                                          // floor(n / 4) = 3
-                                          farEAX = 12, // mod 4 = 0
-                                          farEBX = 13, // mod 4 = 1
-                                          farECX = 14, // mod 4 = 2
-                                          farEDX = 15, // mod 4 = 3
-
-                                                       // Stack registers
-                                                       // floor(n / 4) = 4
-                                                       farESP = 16, // mod 4 = 0
-                                                       farEBP = 17  // mod 4 = 1
+        farDL = 3 // mod 4 = 3
+        ,
+        // Significant byte of lower byte-pair 0000[00]00
+        // floor(n / 4) = 1
+        farAH = 4, // mod 4 = 0
+        farBH = 5, // mod 4 = 1
+        farCH = 6, // mod 4 = 2
+        farDH = 7 // mod 4 = 3
+        ,
+        // Two most significant bytes [0000]0000
+        // floor(n / 4) = 2
+        farAX = 8,  // mod 4 = 0
+        farBX = 9,  // mod 4 = 1
+        farCX = 10, // mod 4 = 2
+        farDX = 11 // mod 4 = 3
+        ,
+        // All four bytes [00000000]
+        // floor(n / 4) = 3
+        farEAX = 12, // mod 4 = 0
+        farEBX = 13, // mod 4 = 1
+        farECX = 14, // mod 4 = 2
+        farEDX = 15 // mod 4 = 3
+        ,
+        // Stack registers
+        // floor(n / 4) = 4
+        farESP = 16, // mod 4 = 0
+        farEBP = 17  // mod 4 = 1
     };
 
     enum Instruction {
         faiUnknown = 0,
 
         // Move instructions (0xA0 - 0xA5)
-        faiMOV_REG_CONST = 0xa0,
-        faiMOV_REG_REG = 0xa1,
-        faiMOV_REG_ADDR = 0xa2,
-        faiMOV_ADDR_CONST = 0xa3,
-        faiMOV_ADDR_REG = 0xa4,
+        // Move instructions (0xA0 - 0xA5)
+        faiMOV_REG_CONST = 0xa0, // Move instructions (0xA0 - 0xA5)
+        faiMOV_REG_REG = 0xa1, // Move instructions (0xA0 - 0xA5)
+        faiMOV_REG_ADDR = 0xa2, // Move instructions (0xA0 - 0xA5)
+        faiMOV_ADDR_CONST = 0xa3, // Move instructions (0xA0 - 0xA5)
+        faiMOV_ADDR_REG = 0xa4, // Move instructions (0xA0 - 0xA5)
         faiMOV_ADDR_ADDR = 0xa5,
 
         // Stack instructions (0xAC - 0xAF)
@@ -113,68 +109,73 @@ namespace FlexASM
 
         // Math instructions (0xE0 - 0xEF)
         // Unsigned
-        faiADD_REG_CONST = 0xe0,
-        faiADD_REG_REG = 0xe1,
-        faiSUB_REG_CONST = 0xe2,
-        faiSUB_REG_REG = 0xe3,
-        faiMUL_REG_CONST = 0xe4,
-        faiMUL_REG_REG = 0xe5,
-        faiDIV_REG_CONST = 0xe6,
-        faiDIV_REG_REG = 0xe7,
+        faiADD_REG_CONST = 0xE0,
+        faiADD_REG_REG = 0xE1,
+        faiSUB_REG_CONST = 0xE2,
+        faiSUB_REG_REG = 0xE3,
+        faiMUL_REG_CONST = 0xE4,
+        faiMUL_REG_REG = 0xE5,
+        faiDIV_REG_CONST = 0xE6,
+        faiDIV_REG_REG = 0xE7,
 
         // signed
-        faiIADD_REG_CONST = 0xe8,
-        faiIADD_REG_REG = 0xe9,
-        faiISUB_REG_CONST = 0xea,
-        faiISUB_REG_REG = 0xeb,
-        faiIMUL_REG_CONST = 0xec,
-        faiIMUL_REG_REG = 0xed,
-        faiIDIV_REG_CONST = 0xee,
-        faiIDIV_REG_REG = 0xef,
+        faiIADD_REG_CONST = 0xE8,
+        faiIADD_REG_REG = 0xE9,
+        faiISUB_REG_CONST = 0xEA,
+        faiISUB_REG_REG = 0xEB,
+        faiIMUL_REG_CONST = 0xEC,
+        faiIMUL_REG_REG = 0xED,
+        faiIDIV_REG_CONST = 0xEE,
+        faiIDIV_REG_REG = 0xEF,
 
-        faiCMP_REG_CONST = 0xf0,
-        faiCMP_REG_REG = 0xf1,
+        faiCMP_REG_CONST = 0xF0,
+        faiCMP_REG_REG = 0xF1,
 
-        faiDBG_REG = 0xfe,
-        faiDBG_ADDR = 0xff,
+        faiDBG_REG = 0xFE,
+        faiDBG_ADDR = 0xFF,
+        
+        // pseude instructions reserved (0x10 - 0x2F)
 
-
-        // pseude instructions (0x30 - 0x3f)
+        // pseude instructions (0x30 - 0x3F)
+        // 1 byte operations (0x30 - 0x34)
         fapiRESB = 0x30, // reserves an amount of bytes
-        fapiRESW = 0x31, // reserves an amount of words
-        fapiRESD = 0x32, // reserves an amount of double words
+        fapiDB = 0x31  // defines a byte
+        ,
+        // 2 bytes operations (0x35 - 0x39)
+        fapiRESW = 0x35, // reserves an amount of words
+        fapiDW = 0x36  // defines a word
+        ,
+        // 4 bytes operations (0x3A - 0x3F)
+        fapiRESD = 0x3A, // reserves an amount of double words
+        fapiDD = 0x3B,  // defines a double word
 
-        fapiDB = 0x33, // defines a byte
-        fapiDBA = 0x34, // defines an array of bytes
-        fapiDW = 0x36, // defines a word
-        fapiDWA = 0x37, // defines an array of words
-        fapiDD = 0x39,  // defines a double word
-        fapiDDA = 0x3A  // defines an array of double words
     };
 
     // Valid registers for the assembler
-    struct ValidRegisterStruct
-    {
+    struct ValidRegisterStruct {
         std::string Name;
         Register Register;
     };
+
     extern std::vector<ValidRegisterStruct> ValidRegisters;
-    void InitValidRegisters();
     bool IsValidRegister(const std::string string);
 
+    void InitValidRegisters();
+
     // Valid instructions for the assembler
-    struct ValidInstructionStruct
-    {
+    struct ValidInstructionStruct {
         std::string Pattern;
         Instruction Instruction;
     };
+
     extern std::vector<ValidInstructionStruct> ValidInstructions;
-    void InitValidInstructions();
     bool IsValidInstruction(const std::string pattern);
     bool IsValidInstructionMnemonic(const std::string mnemonic);
 
+    extern std::vector<ValidInstructionStruct> ValidPseudoInstructions;
+    bool IsValidPseudoInstruction(const std::string pattern);
+
+    void InitValidInstructions();
+} // namespace FlexASM
 
 
-
-
-}
