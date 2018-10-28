2df8840bd52e282caa9df9dfecbd5e1b7ff8e746
pointermess <deskbuddybeta@gmail.com>
2018-10-29 00:47:55 +0100

    new exceptions and reworked base exception a little bit

--- src/FAExceptions.cpp
+++ src/FAExceptions.cpp
@@ -1,14 +1,50 @@
 #include "stdafx.h"
 #include "FAExceptions.h"
 
-FlexASM::ParserUnexpectedTokenException::ParserUnexpectedTokenException(FAToken token)
+
+FlexASM::ParserUnexpectedTokenException::ParserUnexpectedTokenException(FAToken token, const std::string expected)
+    : Exception(token)
 {
-    _token = token;
-    
+    _expected = expected;
 }
 
 
 std::string FlexASM::ParserUnexpectedTokenException::GetMessage()
 {
-    return "[Error] ("+ std::to_string(_token.Line) + ", " + std::to_string(_token.Position) + ") Unexpected token '" + _token.Value + "'.";
+    std::string message = "[Error] (" + std::to_string(_token.Line) + ", " + std::to_string(_token.Position) + ") Unexpected token '" + _token.Value + "'";
+
+    if (!_expected.empty())
+        message += ", expected '" + _expected + "'";
+
+    return message + ".";
+}
+
+FlexASM::ParserIdentifierRedefinedException::ParserIdentifierRedefinedException(FAToken token, const std::string variableName)
+    : Exception(token)
+{
+    _token = token;
+    _variableName = variableName;
+}
+
+std::string FlexASM::ParserIdentifierRedefinedException::GetMessage()
+{
+    return "[Error] (" + std::to_string(_token.Line) + ", " + std::to_string(_token.Position) + ") Identifier '" + _variableName + "' already in use.";
+}
+
+FlexASM::Exception::Exception(FAToken token)
+{
+    _token = token;
+}
+
+FlexASM::ParserInvalidPseudoInstructionException::ParserInvalidPseudoInstructionException(FAToken token, const std::string instructionName)
+    : Exception(token)
+{
+    _token = token;
+    _instructionName = instructionName;
+}
+
+std::string FlexASM::ParserInvalidPseudoInstructionException::GetMessage()
+{
+    return "[Error] (" + std::to_string(_token.Line) + ", " + std::to_string(_token.Position) + ") Pseudo-Instruction '" + _instructionName + "' is invalid.";
+
 }
--- src/FAExceptions.h
+++ src/FAExceptions.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <exception>
+#include <exception> 
 #include <string>
 #include "FAToken.h"
 
@@ -9,19 +9,44 @@ namespace FlexASM
 
     class Exception : public std::exception
     {
+    protected:
+        FAToken _token;
 
     public:
+        Exception(FAToken token);
 
+        virtual std::string GetMessage() = 0;
 
     };
 
     class ParserUnexpectedTokenException : public Exception
     {
     private:
-        FAToken _token;
+        std::string _expected;
     public:
-        ParserUnexpectedTokenException(FAToken token);
+        ParserUnexpectedTokenException(FAToken token, const std::string expected);
+        ParserUnexpectedTokenException(FAToken token) : Exception(token) {};
 
+        std::string GetMessage();
+    };
+
+    class ParserIdentifierRedefinedException : public Exception
+    {
+    private:
+        FAToken _token;
+        std::string _variableName;
+    public:
+        ParserIdentifierRedefinedException(FAToken token, const std::string variableName);
+        std::string GetMessage();
+    };
+
+    class ParserInvalidPseudoInstructionException : public Exception
+    {
+    private:
+        FAToken _token;
+        std::string _instructionName;
+    public:
+        ParserInvalidPseudoInstructionException(FAToken token, const std::string instructionName);
         std::string GetMessage();
     };
 
