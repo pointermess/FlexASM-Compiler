b092ac549ceb895523e021921ea288b6eb325743
Milos Stanojevic <37949266+pointermess@users.noreply.github.com>
2018-11-30 20:30:54 +0100

    specify input and output file by using start parameters
    you can drag n drop a flexasm source file into the compiler now and it will generate a binary in the same folder with the same name but .fae file extension

--- src/FlexASM Compiler.cpp
+++ src/FlexASM Compiler.cpp
@@ -16,16 +16,79 @@
 
 #include "FAProgramData.h"
 #include "FAProgramCode.h"
+#include "FABinaryGenerator.h"
+
+#include <fstream>
+#include <filesystem>
 
 
-int main()
+void ParseAndGenerate(std::string inputPath, std::string outputPath)
 {
-    FlexASM::InitValidRegisters();
-    
     auto parser = std::make_unique<FlexASM::Parser>();
-    parser->ParseFile("D:/asm/cpp/main.txt");
-    
-    getchar();
-    return 0;
+    auto program = parser->ParseFile(inputPath);
+
+    if (program != nullptr)
+    {
+        auto generator = std::make_shared<FABinaryGenerator>();
+        const std::vector<char> stream = generator->CreateBinaryStream(program);
+
+
+        std::ofstream writeFile;
+        writeFile.open(outputPath, std::ios::out);
+        writeFile.flush();
+        writeFile.write(&stream[0], stream.size());
+        writeFile.close();
+    }
 }
 
+int main(int argc, char *argv[])
+{
+
+    FlexASM::InitValidRegisters();
+    FlexASM::InitValidInstructions();
+
+    std::cout << "FlexASM Compiler Version 0.1" << std::endl << std::endl;
+
+    if (argc < 2)
+    {
+        std::filesystem::path binName = argv[0];
+
+        std::cout << "Following run parameters are supported:" << std::endl;
+        std::cout << "- " << binName.filename() << " INPUT_FILE" << std::endl;
+        std::cout << "- " << binName.filename() << " INPUT_FILE_PATH OUTPUT_FILE_PATH" << std::endl;
+    }
+    else if (argc == 2)
+    {
+        std::filesystem::path inputPath(argv[1]);
+        inputPath = std::filesystem::absolute(inputPath);
+
+        if (std::filesystem::exists(inputPath))
+        {
+            std::filesystem::path path = inputPath.root_path();
+            path /= inputPath.parent_path();
+            path /= inputPath.stem();
+            path += ".fae";
+
+            ParseAndGenerate(inputPath.u8string(), path.u8string());
+
+            std::cout << "Compilation finished." << std::endl;
+            std::cout << "Created binary at " << path << std::endl;
+        }
+    }
+    else if (argc == 3)
+    {
+        std::filesystem::path inputPath(argv[1]);
+        inputPath = std::filesystem::absolute(inputPath);
+        std::filesystem::path outputPath(argv[2]);
+        outputPath = std::filesystem::absolute(outputPath);
+
+        if (std::filesystem::exists(inputPath))
+        {
+            ParseAndGenerate(inputPath.u8string(), outputPath.u8string());
+
+            std::cout << "Created binary at " << outputPath << std::endl;
+        }
+    }
+
+    return 0;
+}
